2103a05488135637bdc9e3946116bc76985ae36d
Arash Javanmard <a.javan.gh@gmail.com>
2019-10-09 17:48:29 +0200

    impl. cont.

--- lidar.py
+++ lidar.py
@@ -1,8 +1,12 @@
 import os
+import datetime
 import argparse
 import dpkt
 import math
 import numpy as np
+
+from gps import GprmcMessage, utc_to_weekseconds
+
 
 def read_uint8(data, idx):
   return data[idx]
@@ -25,21 +29,6 @@ def read_sint16(data, idx):
 def read_uint32(data, idx):
   return data[idx] + data[idx+1]*256 + data[idx+2]*256*256 + data[idx+3]*256*256*256
 
-_RotSinTab = {}
-_RotCosTab = {}
-
-for i in range(360*100):
-  _RotSinTab[i/100] = math.sin(math.radians(i/100))
-  _RotCosTab[i/100] = math.cos(math.radians(i/100))
-
-class Point():
-    def __init__(self):
-        self.x = 0.
-        self.y = 0.
-        self.z = 0.
-        self.intensity = 0.
-        self.laser_id = 0.
-        self.timestamp = 0.
 
 class Lidar():
     def __init__(self):
@@ -53,13 +42,18 @@ class Lidar():
 
 
 class VelodyneVLP16(Lidar):
+    # factor distance centimeter value to meter
+    FACTOR_CM2M = 0.01
+
+    # factor distance value to cm, each velodyne distance unit is 2 mm
+    FACTOR_MM2CM = 0.2
+
     def __init__(self, dual_mode=False):
         super(VelodyneVLP16, self).__init__()
         self.dual_mode = dual_mode
         self.timing_offsets = self.calc_timing_offsets()
 
-        self.omega = np.array([-15, 1, -13, 3, 11, 5, -9, 7, -7, 9, -5, 11, -3, 13, -1, 15])
-
+        self.omega = np.array([-15, 1, -13, 3, -11, 5, -9, 7, -7, 9, -5, 11, -3, 13, -1, 15])
         self.count_lasers = 16
 
     def calc_timing_offsets(self):
@@ -72,9 +66,9 @@ class VelodyneVLP16(Lidar):
         for x in range(12):
             for y in range(32):
                 if self.dual_mode:
-                    dataBlockIndex = (x - (x % 2)) + (y / 16)
+                    dataBlockIndex = (x - (x % 2)) + int((y / 16))
                 else:
-                    dataBlockIndex = (x * 2) + (y / 16)
+                    dataBlockIndex = (x * 2) + int((y / 16))
                 dataPointIndex = y % 16
                 timing_offsets[y][x] = \
                     (full_firing_cycle * dataBlockIndex) + (single_firing * dataPointIndex)
@@ -86,6 +80,8 @@ class VelodyneVLP16(Lidar):
         :param frame_idx:
         :return: X,Y,Z-coordinate, azimuth, intensitiy, timestamp of each firing, sequence ordered, shape of each = [384x1]
         """
+        data = np.frombuffer(data, dtype=np.uint8).astype(np.uint32)
+
         # frame length = 1206
         timestamp = read_uint32(data, 1200)
         factory = data[1204:]
@@ -124,16 +120,80 @@ class VelodyneVLP16(Lidar):
         intensities = intensities.flatten()
         azimuth = azimuth.flatten()
         timestamps = timestamps.flatten()
+        distances = distances.flatten() * self.FACTOR_MM2CM * self.FACTOR_CM2M
 
-        return X, Y, Z, intensities, azimuth, timestamps
+        # remeber the last azimuth for roll over checking
+        self.cur_azimuth = azimuth
+
+        return X, Y, Z, intensities, azimuth, timestamps, distances
 
     def process_position_frame(self, data, frame_idx):
-        raise NotImplementedError("Subclasses should implement this!")
+        timestamp = data[198:202]
+        timestamp = read_uint32(timestamp, 0)
+
+        ppps_status = data[202]
+
+        gprmc = data[206:]
+        gprmc = gprmc.split()[0]  # filter out gprmc message, remaining are zeros
+        gprmc = gprmc.decode('ascii').split(',')  # convert bytes array to string
+
+        gps_msg = GprmcMessage()
+        time = gprmc[1]
+        date = gprmc[9]
+        gps_msg.datetime = datetime.datetime.strptime(date + time, '%d%m%y%H%M%S')
+
+        gps_msg.status = gprmc[2]
+        gps_msg.lat = float(gprmc[3])
+        gps_msg.lat_ori = gprmc[4]
+        gps_msg.long = float(gprmc[5])
+        gps_msg.long_ori = gprmc[6]
+        gps_msg.velocity = float(gprmc[7])
+        gps_msg.course = float(gprmc[8])
+
+        gps_msg.mag = float(gprmc[10])
+        gps_msg.mag_sign = gprmc[11]
+        gps_msg.singularity = gprmc[12]
+        gps_msg.weeks, _, gps_msg.seconds, _ = utc_to_weekseconds(gps_msg.datetime, leapseconds=0)
+
+        return gps_msg
+
 
     def calc_precise_azimuth(self, azimuth):
+        """
+        Linear interpolation of azimuth values
+        :param azimuth:
+        :return:
+        """
+        org_azi = azimuth.copy()
+
         precision_azimuth = []
         # iterate through each block
         for n in range(12): # n=0..11
+            azimuth = org_azi.copy()
+            try:
+                # First, adjust for an Azimuth rollover from 359.99° to 0°
+                if azimuth[n + 1] < azimuth[n]:
+                    azimuth[n + 1] += 360.
+
+                # Determine the azimuth Gap between data blocks
+                azimuth_gap = azimuth[n + 1] - azimuth[n]
+            except:
+                azimuth_gap = azimuth[n] - azimuth[n-1]
+
+            factor = azimuth_gap / 32.
+            k = np.arange(32)
+            precise_azimuth = azimuth[n] + factor * k
+            precision_azimuth.append(precise_azimuth)
+        precision_azimuth = np.array(precision_azimuth)
+        return precision_azimuth
+
+    def calc_precise_azimuth_2(self, azimuth):
+        org_azi = azimuth.copy()
+
+        precision_azimuth = []
+        # iterate through each block
+        for n in range(12): # n=0..11
+            azimuth = org_azi.copy()
             try:
                 # First, adjust for an Azimuth rollover from 359.99° to 0°
                 if azimuth[n + 1] < azimuth[n]:
@@ -152,9 +212,13 @@ class VelodyneVLP16(Lidar):
                     precise_azimuth = azimuth[n] + (azimuth_gap * 2.304 * k) / 55.296
                 else:
                     # interpolate
-                    precise_azimuth = azimuth[n] + (azimuth_gap * 2.304 * ((k-16) + 55.296)) / (2 * 55.296);
+                    precise_azimuth = azimuth[n] + (azimuth_gap * 2.304 * ((k-16) + 55.296)) / 55.296
+                if precise_azimuth > 361.:
+                    print("Error")
+                print(precise_azimuth)
                 precision_azimuth.append(precise_azimuth)
-        return np.array(precision_azimuth)
+        precision_azimuth = np.array(precision_azimuth)
+        return precision_azimuth
 
     def read_firing_data(self, data):
         block_id = data[0] + data[1]*256
@@ -169,17 +233,11 @@ class VelodyneVLP16(Lidar):
         return distances, intensities, azimuth
 
     def calc_cart_coord(self, distances, azimuth):
-        # factor distance centimeter value to meter
-        FACTOR_CM2M = 0.01
-
-        # factor distance value to cm, each velodyne distance unit is 2 mm
-        FACTOR_MM2CM = 0.2
-
         # convert distances to meters
-        distances = distances * FACTOR_MM2CM * FACTOR_CM2M
+        distances = distances * self.FACTOR_MM2CM * self.FACTOR_CM2M
 
         # convert deg to rad
-        longitudes = np.repeat(self.omega * np.pi / 180., 2)
+        longitudes = np.tile(self.omega * np.pi / 180., 2)
         latitudes = azimuth * np.pi / 180.
 
         hypotenuses = distances * np.cos(longitudes)
@@ -187,5 +245,4 @@ class VelodyneVLP16(Lidar):
         X = hypotenuses * np.sin(latitudes)
         Y = hypotenuses * np.cos(latitudes)
         Z = distances * np.sin(longitudes)
-
         return X, Y, Z
--- lidar_manager.py
+++ lidar_manager.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import dpkt
 import datetime
 import numpy as np
-import pypcd
-from  pypcd import pypcd
+
+from gps import GprmcMessage
 import lidar
 
 
@@ -15,22 +15,39 @@ def create_point_cloud(X, Y, Z):
 def write_point_cloud_to_file(path, index, X, Y, Z, intensities, latitudes, longitudes, timestamps):
     pass
 
+
 class VelodyneManager():
 
-    def __init__(self, type, pcap_path, out_path, params):
+    def __init__(self, type, pcap_path, out_root, params):
         self.pcap_path = Path(pcap_path)
         self.params = params
         self.lidar_type = type
-        self.out_path = Path("{}/{}".format(out_path, self.lidar_type.lower()))
         self.lidar = None
+        self.out_root = out_root
+
+        self.pos_X = None
+        self.pos_Y = None
+        self.pos_Z = None
+        self.intensities = None
+        self.latitudes = None
+        self.timestamps = None
+        self.distances = None
+        self.indicies = None
+        self.longitudes = None
+
+        self.frame_nr = 0
+        self.cur_azimuth = None
+        self.last_azimuth = None
+        self.date = None
+
         if "velodynevlp16" == type.lower():
             self.lidar = lidar.VelodyneVLP16()
 
-        if params['text']:
-            txt_path = "{}/pcl_{}.txt".format(self.out_path, datetime.datetime.now())
-            self.dframe_txt_writer = PointCloudToText(txt_path)
-
     def run(self):
+        """
+        Exteractis point clouds from pcap file
+        :return:
+        """
 
         # open pcap file
         try:
@@ -41,89 +58,256 @@ class VelodyneManager():
             return
 
         # create output folder hierarchy
-        self.create_folders()
+        if not self.create_folders():
+            return
+
+        # create frame text writer
+        if self.params['text']:
+            self.frame_writer = PointCloudTextWriter(self.txt_path, index=self.frame_nr)
 
         # itearte through each data packet and timestamps
         for idx, (ts, buf) in enumerate(self.lidar_reader):
 
             if idx < self.params['from']:
                 continue
-            if self.params['to'] > 0 and idx > self.params['to']:
+            if 0 < self.params['to'] < idx:
                 break
+
+            if self.date is None:
+                self.date = datetime.datetime.utcfromtimestamp(ts).date()
 
             eth = dpkt.ethernet.Ethernet(buf)
             data = eth.data.data.data
 
-            data = np.frombuffer(data, dtype=np.uint8).astype(np.uint32)
-
+            # handle Position-Frame (GPS-Data)
             if self.params['gps']:
                 if eth.data.data.sport == self.params['gps-port']:
-                    self.lidar.process_position_frame(data, idx)
+                    self.process_gps_frame(data, ts, idx)
 
+            # Handle Data-Frame (Point clouds)
             if eth.data.data.sport == self.params['data-port']:
                 self.process_data_frame(data, ts, idx)
 
     def process_data_frame(self, data, timestamp, index):
-        X, Y, Z, intensities, latitudes, timestamps = self.lidar.process_data_frame(data, index)
+        cur_X, cur_Y, cur_Z, cur_intensities, cur_latitudes, cur_timestamps, cur_distances = self.lidar.process_data_frame(data, index)
 
         # number of sequences
-        n_seq = int(len(X) / self.lidar.count_lasers)
+        n_seq = int(len(cur_X) / self.lidar.count_lasers)
 
-        indicies = np.repeat(np.arange(self.lidar.count_lasers), n_seq)
-        longitudes = np.repeat(self.lidar.omega, n_seq)
+        cur_indicies = np.tile(np.arange(self.lidar.count_lasers), n_seq)
+        cur_longitudes = np.tile(self.lidar.omega, n_seq)
 
-        # wrtie point cloud as a text-file
-        self.dframe_txt_writer.write(indicies, X, Y, Z, timestamps, intensities, latitudes, longitudes)
+        # initilaise states
+        if index == 0 or self.pos_X is None:
+            self.pos_X = cur_X
+            self.pos_Y = cur_Y
+            self.pos_Z = cur_Z
+            self.intensities = cur_distances
+            self.latitudes = cur_latitudes
+            self.timestamps = cur_timestamps
+            self.distances = cur_distances
+            self.indicies = cur_indicies
+            self.longitudes = cur_longitudes
 
-    def create_folders(self):
-        try:
-            os.makedirs(self.outpath.absolute())
-        except Exception as ex:
-            print(str(ex))
+
+        if self.cur_azimuth is None:
+            self.cur_azimuth = cur_latitudes
+            self.last_azimuth = cur_latitudes
+
+        # update current azimuth before checking for roll over
+        self.cur_azimuth = cur_latitudes
+
+        # check if a frame is finished
+        idx_rollovr = self.is_roll_over()
+
+        # handle rollover (full 360° frame)
+        if idx_rollovr is not None:
+            time = self.time_from_lidar(self.timestamps[0])
+
+            if idx_rollovr > 0:
+                self.pos_X = np.hstack((self.pos_X, cur_X[0:idx_rollovr+1]))
+                self.pos_Y = np.hstack((self.pos_Y, cur_Y[0:idx_rollovr+1]))
+                self.pos_Z = np.hstack((self.pos_Z, cur_Z[0:idx_rollovr+1]))
+                self.intensities = np.hstack((self.intensities, cur_intensities[0:idx_rollovr+1]))
+                self.latitudes = np.hstack((self.latitudes, cur_latitudes[0:idx_rollovr+1]))
+                self.timestamps = np.hstack((self.timestamps, cur_timestamps[0:idx_rollovr+1]))
+                self.distances = np.hstack((self.distances, cur_distances[0:idx_rollovr+1]))
+                self.indicies = np.hstack((self.indicies, cur_indicies[0:idx_rollovr+1]))
+                self.longitudes = np.hstack((self.longitudes, cur_longitudes[0:idx_rollovr+1]))
+
+            if self.params['text']:
+
+                fpath = "{}/{}_data_frame.txt".format(self.txt_path, index)
+                write_pcl_txt(self.txt_path, date, self.timestamps[0], self.frame_nr, self.timestamps,
+                              self.pos_X, self.pos_Y, self.pos_Z, self.indicies,
+                              self.intensities, self.latitudes, self.longitudes, self.distances)
+            if idx_rollovr > 0:
+                self.pos_X = cur_X[idx_rollovr+1:]
+                self.pos_Y = cur_Y[idx_rollovr+1:]
+                self.pos_Z = cur_Z[idx_rollovr+1:]
+                self.intensities = cur_intensities[idx_rollovr+1:]
+                self.latitudes = cur_latitudes[idx_rollovr+1:]
+                self.timestamps = cur_timestamps[idx_rollovr+1:]
+                self.distances = cur_distances[idx_rollovr+1:]
+                self.indicies = cur_indicies[idx_rollovr+1:]
+                self.longitudes = cur_longitudes[idx_rollovr+1:]
+            else:
+                self.pos_X = cur_X
+                self.pos_Y = cur_Y
+                self.pos_Z = cur_Z
+                self.intensities = cur_intensities
+                self.latitudes = cur_latitudes
+                self.timestamps = cur_timestamps
+                self.distances = cur_distances
+                self.indicies = cur_indicies
+                self.longitudes = cur_longitudes
+
+            self.frame_nr += 1
+
+            # reset roll over check
+            self.cur_azimuth = None
             return
 
-        # create point cloud path
-        self.pcl_path = Path("{}/{}".format(self.outpath, "pointlcouds"))
+        self.pos_X = np.hstack((self.pos_X, cur_X))
+        self.pos_Y = np.hstack((self.pos_Y, cur_Y))
+        self.pos_Z = np.hstack((self.pos_Z, cur_Z))
+        self.intensities = np.hstack((self.intensities, cur_intensities))
+        self.latitudes = np.hstack((self.latitudes, cur_latitudes))
+        self.timestamps = np.hstack((self.timestamps, cur_timestamps))
+        self.distances = np.hstack((self.distances, cur_distances))
+        self.indicies = np.hstack((self.indicies, cur_indicies))
+        self.longitudes = np.hstack((self.longitudes, cur_longitudes))
+
+        self.last_azimuth = cur_latitudes
+
+    def process_gps_frame(self, data, timestamp, index):
+        gps_msg = self.lidar.process_position_frame(data, index)
+
+        if self.params['text']:
+            # write point cloud as a text-file
+            write_gps_txt(self.txt_path, timestamp, index, gps_msg)
+
+    def is_roll_over(self):
+
+        diff_cur = self.cur_azimuth[0:-1] - self.cur_azimuth[1:]
+        diff_cur_last = self.cur_azimuth - self.last_azimuth
+
+        res_cur = np.where(diff_cur > 0.)[0]
+        res_cur_last =  np.where(diff_cur_last < 0.)[0]
+        if res_cur.size > 0:
+            index = res_cur[0]
+            return index
+        elif res_cur_last.size > 0:
+            index = res_cur_last[0]
+            return index
+        else:
+            return None
+
+    def time_from_lidar(self, timestamp):
+        pass
+
+    def create_folders(self):
+        self.out_path = Path("{}/{}".format(self.out_root, self.lidar_type.lower()))
+
+        # creating output dir
+        try:
+            os.makedirs(self.out_path.absolute())
+        except Exception as ex:
+            print(str(ex))
+            return False
+
+        # create point cloud dirs
+        self.pcl_path = Path("{}/{}".format(self.out_path, "frames_pcl"))
         try:
             os.makedirs(self.pcl_path.absolute())
         except Exception as ex:
             print(str(ex))
-            return
+            return False
+
+        # if text-files are desired, create text-file dir
+        if self.params['text']:
+            self.txt_path = Path("{}/{}".format(self.out_path, "frames_text"))
+            try:
+                os.makedirs(self.txt_path.absolute())
+            except Exception as ex:
+                print(str(ex))
+                return False
+        return True
 
 
-class PointCloudToText():
-    def __init__(self, path):
+def write_gps_txt(path, ts_frame, index, gps_msg):
+    fpath = "{}/{}_{}_{}.txt".format(path, index, "gps", str(datetime.datetime.utcfromtimestamp(ts_frame)))
+    header = "UTC-Time, Week, Seconds [sow], Status, Latitude [Deg], Longitudes [Deg], Velocity [m/s]\n"
+    txt = "{}, {}, {}, {}, {} {}, {} {}, {}\n".format(gps_msg.datetime, gps_msg.weeks, gps_msg.seconds,
+                                                      gps_msg.status, gps_msg.lat, gps_msg.lat_ori,
+                                                      gps_msg.long, gps_msg.lat_ori, gps_msg.velocity)
+    with open(fpath, 'w') as fp:
+        fp.write(header)
+        fp.write(txt)
+
+
+def write_pcl_txt(path, ts_frame, index, timestamps, X, Y, Z,  laser_id, intensities=None, latitudes=None, longitudes=None, distances=None):
+    header = "Time [musec], X [m], Y [m], Z [m], ID, Intensity, Latitude [Deg], Longitudes [Deg], Distance [m]"
+    fpath = "{}/{}_data_frame.txt".format(path,  index)
+
+    try:
+        fp = open(fpath, 'w')
+        np.savetxt(fp, [], delimiter=', ', header=header)
+    except Exception as ex:
+        print(str(ex))
+        return
+
+    M = np.vstack((timestamps, X, Y, Z, laser_id))
+
+    if intensities is not None:
+        M = np.vstack((M, intensities))
+    if latitudes is not None:
+        M = np.vstack((M, latitudes))
+    if longitudes is not None:
+        M = np.vstack((M, longitudes))
+    if distances is not None:
+        M = np.vstack((M, distances))
+
+    np.savetxt(fp, M.T, fmt=('%d', '%.6f', '%.6f', '%.6f', '%d', '%d', '%.3f', '%.3f', '%.3f'), delimiter=', ')
+    fp.close()
+
+
+class PointCloudTextWriter():
+    def __init__(self, path, index):
         self.path = path
         self.initialised = False
-
+        self.index = index
 
     def initialise(self):
+        header = "Time [musec], X [m], Y [m], Z [m], ID, Intensity, Latitude [Deg], Longitudes [Deg], Distance [m]"
+        fpath = "{}/{}_{}.txt".format(self.path, self.index, "data_frame")
 
-        header = "Id, X [m], Y, Z, Time [musec], Intensity, Latitude [Deg], Longitudes"
         try:
-            self.fp = open(self.path, 'w')
-            np.savetxt(self.fp, [], delimiter=',', header=header)
+            self.fp = open(self.fpath, 'w')
+            np.savetxt(self.fp, [], delimiter=', ', header=header)
         except Exception as ex:
             print(str(ex))
             return
 
         self.initialised = True
 
-    def write(self, laser_id, X, Y, Z,  timestamps, intensities=None, latitudes=None, longitudes=None):
+    def write(self, timestamps, X, Y, Z,  laser_id, intensities=None, latitudes=None, longitudes=None, distances=None):
 
         if not self.initialised:
             self.initialise()
 
-        T = np.hstack((laser_id, X, Y, Z, timestamps))
+        M = np.vstack((timestamps, X, Y, Z, laser_id))
 
-        if intensities:
-            T = np.vstack((T, intensities))
-        if latitudes:
-            T = np.vstack((T, intensities))
-        if longitudes:
-            T = np.vstack((T, intensities))
+        if intensities is not None:
+            M = np.vstack((M, intensities))
+        if latitudes is not None:
+            M = np.vstack((M, latitudes))
+        if longitudes is not None:
+            M = np.vstack((M, longitudes))
+        if distances is not None:
+            M = np.vstack((M, distances))
 
-        np.savetxt(self.fp, T, delimiter=',')
+        np.savetxt(fp, M.T, fmt=('%d', '%.6f', '%.6f', '%.6f', '%d', '%d', '%.3f', '%.3f', '%.3f'), delimiter=', ')
 
     def close(self):
         self.fp.close()
